736262b4a4ec783ca51efd0a05425d13536b423b
Brandon <bmiller3822@gmail.com>
2020-03-04 17:43:44 -0500

    Base URL, path, and views set up.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Habit, Unit, Log
+
+def habits(request):
+    habits = Habit.objects.all()
+    return render(request, 'core/habits.html', {'habits': habits})
+
 # Create your views here.
