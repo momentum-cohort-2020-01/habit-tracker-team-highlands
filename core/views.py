b91c08418b19a3f83f29d169eacf7a689ee41b68
Brandon <bmiller3822@gmail.com>
2020-03-04 20:51:25 -0500

    Fixing the linter.

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from .models import Habit, Unit, Log
 
+
 def habits(request):
     habits = Habit.objects.all()
     return render(request, 'core/habits.html', {'habits': habits})
