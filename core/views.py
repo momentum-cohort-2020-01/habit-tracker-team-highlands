a5ad0ca82acd9463ab2ec6faa99e650b0f1c3073
Brandon <bmiller3822@gmail.com>
2020-03-06 17:36:34 -0500

    Add user unit creation.

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Habit, Unit, Log, User
+from .models import Habit, Log, User
 
 from .forms import HabitForm, ActivityForm
 
